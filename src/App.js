c5af32c531314194fc3a4eb81cc944b478836310
tahir kalim <thrkalim@gmail.com>
2021-04-22 22:40:20 +0200

    provider setup

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import CartContainer from "./components/CartContainer";
 // items
 import cartItems from "./cart-items";
 // redux stuff
+
 // reducer- function that used to update store
 // two arguments - state, action
 // state - old state/state before update
@@ -12,15 +13,36 @@ import cartItems from "./cart-items";
 // return updated or old state
 
 import {createStore} from 'redux'
+import {DECREASE, INCREASE} from './actions'
+import reducer from './reducer'
+// react-redux  - Provider -  wraps app, connect - used in components
+import {Provider} from 'react-redux'
+// dispatch method - send actions to the store
+// actions (objects) - must have type property - what kind of action
+// dont mutate the state - redux built on immutability
+
+// store.getState() - 
+
 // initial store
 const initialStore = {
-  count: 78
+cart:cartItems,
+total:0,
+amount:0
 }
 // reducer
-function reducer(state, action){
-  console.log({state, action})
-  return state
-}
+// function reducer(state, action){
+//   console.log({state, action})
+//   if(action.type === DECREASE){
+//     return {...state,count: state.count -1}
+
+//   }
+//   if(action.type === INCREASE){
+//     return {...state,count: state.count +1}
+
+//   }
+  
+//   return state
+// }
 // store
 const store = createStore(reducer, initialStore)
 console.log(store.getState())
@@ -29,10 +51,11 @@ console.log(store.getState())
 function App(){
   // cart setup
 return (
-    <main>
+    <Provider store={store}>
+      
       <Navbar />
       <CartContainer cart={cartItems} />
-    </main>
+    </Provider>
   );
 }
 
---
+++ src/reducer.js
@@ -0,0 +1,8 @@
+import {DECREASE, INCREASE} from './actions'
+
+function reducer(state, action){
+  
+  return state
+}
+
+export default reducer
